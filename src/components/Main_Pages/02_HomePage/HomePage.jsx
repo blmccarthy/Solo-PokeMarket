8ac0d070a196ed2e243afdeedae7e1268f441407
Brant McCarthy <brant.mccarthy@me.com>
2022-03-16 15:05:39 -0500

    added basic MUI grid to home page content

--- src/components/Main_Pages/02_HomePage/HomePage.jsx
+++ src/components/Main_Pages/02_HomePage/HomePage.jsx
@@ -3,9 +3,6 @@ import { useSelector, useDispatch } from 'react-redux'
 
 import ListingItem from './ListingItem';
 
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-
 // This is one of our simplest components
 // It doesn't have local state
 // It doesn't dispatch any redux actions or display any part of redux state
--- src/components/Main_Pages/02_HomePage/ListingItem.jsx
+++ src/components/Main_Pages/02_HomePage/ListingItem.jsx
@@ -1,6 +1,8 @@
 import { useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 
 function ListingItem({ listing }) {
 
@@ -13,22 +15,28 @@ function ListingItem({ listing }) {
     }
 
     return (
-        
+
         <div
             value={listing.id}
             onClick={handleListingClick}
         >
-            <br />
-            <div className="home_img">
-                <img src={(images.filter(image => image.listing_id == listing.id)[0])?.url} />
-            </div>
-            <div><b>{listing.card_name}</b></div>
-            <div>Set: {listing.set}</div>
-            <div>Condition: {listing.condition}</div>
-            <div>Graded: {listing.graded}</div>
-            <div>Asking Price: {listing.asking_price}</div>
-            <div>Notes: {listing.notes.substring(0, 80)}{listing.notes.length >= 80 && '...'}</div>
-            <br />
+            <Grid container spacing={3}>
+                <Grid item xs={5}>
+                    <br />
+                    <div className="home_img">
+                        <img src={(images.filter(image => image.listing_id == listing.id)[0])?.url} />
+                    </div>
+                </Grid>
+                <Grid item xs={7}>
+                    <div><b>{listing.card_name}</b></div>
+                    <div>Set: {listing.set}</div>
+                    <div>Condition: {listing.condition}</div>
+                    <div>Graded: {listing.graded}</div>
+                    <div>Asking Price: {listing.asking_price}</div>
+                    <div>Notes: {listing.notes.substring(0, 80)}{listing.notes.length >= 80 && '...'}</div>
+                    <br />
+                </Grid>
+            </Grid>
             <hr />
         </div>
     )
