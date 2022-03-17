84fefad1608499de2dadea3e62a2335a9d2a353e
Brant McCarthy <brant.mccarthy@me.com>
2022-03-16 21:19:02 -0500

    basic page created for create new listing

--- src/components/Main_Pages/03_ListingsPage/CreateListingPage.jsx
+++ src/components/Main_Pages/03_ListingsPage/CreateListingPage.jsx
@@ -1,10 +1,24 @@
 
 
+import { useState } from 'react';
+
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+
 
 function CreateListingPage() {
 
     return (
         <>
+            <TextField
+                id="outlined-required"
+                label="Card Name"
+                autoComplete="off"
+                value={cardName}
+                required
+                fullWidth
+            />
         </>
     )
 }
