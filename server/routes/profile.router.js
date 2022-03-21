cc3cbe08df81acb9911a8bc6388e276ebc531a07
Brant McCarthy <brant.mccarthy@me.com>
2022-03-20 20:55:30 -0500

    basic profile framework achieved!

--- server/routes/profile.router.js
+++ server/routes/profile.router.js
@@ -13,10 +13,38 @@ router.get('/listing-count', (req, res) => {
     console.log('userId', userId);
     const queryText = `SELECT COUNT(id) FROM listing WHERE user_id = $1;`;
     pool.query(queryText, [userId]).then(response => {
-      console.log('in MY COUNT .then');
-      res.send(response.rows);
+      console.log('in MY COUNT .then'); // Directly grabs the count
+      res.send(response.rows[0].count);
     }).catch(err => {
       console.log('in MY COUNT .catch');
+      res.sendStatus(500);
+    })
+  });
+
+// GET MY OUTGOING OFFERS COUNT
+router.get('/offers-out-count', (req, res) => {
+    const userId = req.user.id;
+    console.log('userId', userId);
+    const queryText = `SELECT COUNT(id) FROM offer WHERE buyer_user_id = $1 AND status = 'pending';`;
+    pool.query(queryText, [userId]).then(response => {
+      console.log('in OUT COUNT .then'); 
+      res.send(response.rows[0].count); // Directly grabs the count
+    }).catch(err => {
+      console.log('in OUT COUNT .catch');
+      res.sendStatus(500);
+    })
+  });
+
+// GET MY OUTGOING OFFERS COUNT
+router.get('/offers-in-count', (req, res) => {
+    const userId = req.user.id;
+    console.log('userId', userId);
+    const queryText = `SELECT COUNT(id) FROM offer WHERE seller_user_id = $1 AND status = 'pending';`;
+    pool.query(queryText, [userId]).then(response => {
+      console.log('in IN COUNT .then', response.rows); 
+      res.send(response.rows[0].count); // Directly grabs the count
+    }).catch(err => {
+      console.log('in IN COUNT .catch');
       res.sendStatus(500);
     })
   });
--- src/components/Main_Pages/04_ProfilePage/ProfilePage.jsx
+++ src/components/Main_Pages/04_ProfilePage/ProfilePage.jsx
@@ -4,21 +4,22 @@ import { useSelector, useDispatch } from 'react-redux';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
 
 function ProfilePage() {
 
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch({ type: 'FETCH_MY_LISTINGS_COUNT' })
-    dispatch({ type: 'FETCH_MY_LISTINGS_COUNT' })
+    dispatch({ type: 'FETCH_MY_LISTINGS_COUNT' });
+    dispatch({ type: 'FETCH_OFFERS_OUT_COUNT' });
+    dispatch({ type: 'FETCH_OFFERS_IN_COUNT' });
   }, []);
 
-  const user = useSelector(store => store.user)
-  const myListingCount = useSelector(store => store.profile.myListingCountReducer)
+  const user = useSelector(store => store.user);
+  const profile = useSelector(store => store.profile.profileCountReducer);
 
-
-  console.log('myListingCount', myListingCount);
+  console.log('profile', profile);
 
   return (
     <>
@@ -28,20 +29,40 @@ function ProfilePage() {
         sx={{ width: "50%", height: "50%", margin: "auto", mt: 1 }}
       />
       <Typography variant="h5" sx={{ textAlign: 'center', my: 2 }}>Welcome, {user.username}!</Typography>
-      <Grid container spacing={2} sx={{ px: 2 }}>
+
+      <Grid container spacing={2} sx={{ px: 2, mb: 2 }}>
         <Grid item xs={6}>
-          <Typography sx={{ color: '#707070'}}>First Name:</Typography>
+          <Typography sx={{ color: '#707070' }}>First Name:</Typography>
           <Typography sx={{ mb: .75, fontWeight: 500 }}>{user.first_name}</Typography>
-          <Typography sx={{ color: '#707070'}}>Last Name:</Typography>
+          <Typography sx={{ color: '#707070' }}>Last Name:</Typography>
           <Typography sx={{ mb: .75, fontWeight: 500 }}>{user.last_name}</Typography>
-          <Typography sx={{ color: '#707070'}}>Location:</Typography>
+          <Typography sx={{ color: '#707070' }}>Location:</Typography>
           <Typography sx={{ mb: .75, fontWeight: 500 }}>{user.city}, {user.state}</Typography>
         </Grid>
         <Grid item xs={6}>
-          <Typography sx={{ color: '#707070'}}>Active Listings:</Typography>
-          <Typography sx={{ mb: .75, fontWeight: 500 }}>{myListingCount.count}</Typography>
-          <Typography sx={{ color: '#707070'}}>Pending Offers:</Typography>
-          <Typography sx={{ mb: .75, fontWeight: 500 }}>{user.last_name}</Typography>
+          <Typography sx={{ color: '#707070' }}>Active Listings:</Typography>
+          <Typography sx={{ mb: .75, fontWeight: 500 }}>{profile.listing_count}</Typography>
+          <Typography sx={{ color: '#707070' }}>Outgoing Offers:</Typography>
+          <Typography sx={{ mb: .75, fontWeight: 500 }}>{profile.offer_in_count}</Typography>
+          <Typography sx={{ color: '#707070' }}>Incoming Offers:</Typography>
+          <Typography sx={{ mb: .75, fontWeight: 500 }}>{profile.offer_out_count}</Typography>
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <Button
+            variant="outlined"
+            fullWidth
+            sx={{}}>Edit</Button>
+        </Grid>
+        <Grid item xs={6}>
+          <Button
+            variant="outlined"
+            fullWidth
+          >
+            Sign Out
+          </Button>
         </Grid>
       </Grid>
     </>
